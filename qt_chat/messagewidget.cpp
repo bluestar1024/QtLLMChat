edd9bb8d0bcb1210f269dab79aca89b3458c80c1
bulestar1024 <1031489783@qq.com>
2025-12-26 22:20:19 +0800

    聊天部件

--- qt_chat/messagewidget.cpp
+++ qt_chat/messagewidget.cpp
@@ -44,42 +44,48 @@ MessageWidget::MessageWidget(const QString &text, std::function<void()> copyFun,
     aiUpdateSizeTimer.setSingleShot(true);
     connect(&aiUpdateSizeTimer, &QTimer::timeout, this, &MessageWidget::onAiUpdateSize);
 
-    imageLabel = new ImageLabel(isUser, this);
-
-    textWidget = new QWidget(this);
+    imageLabel = new ImageLabel(isUser);
+    textWidget = new TextWidget(isUser);
     textLayout = new QVBoxLayout(textWidget);
-    textLayout->setContentsMargins(15, 5, 15, 5);
+    if (this->isUser)
+        textLayout->setContentsMargins(5, 0, 5, 0);
+    else
+        textLayout->setContentsMargins(15, 5, 15, 5);
 
-    textBoxWidget = new QWidget(this);
+    textBoxWidget = new TextBoxWidget();
     textBoxLayout = new QVBoxLayout(textBoxWidget);
     textBoxLayout->setSpacing(0);
     textBoxLayout->setContentsMargins(0, 0, 0, 0);
 
-    funWidget = new QWidget(this);
-    auto *funLay = new QHBoxLayout(funWidget);
-    funLay->setContentsMargins(5, 5, 5, 5);
-    copyButton = new QPushButton(funWidget);
+    funWidget = new QWidget();
+    auto *funHLayout = new QHBoxLayout(funWidget);
+    funHLayout->setContentsMargins(5, 5, 5, 5);
+    copyButton = new CopyButton("复制", 15, 35, this);
     copyButton->setFixedSize(16, 16);
-    copyButton->setCursor(Qt::PointingHandCursor);
-    copyButton->setToolTip("复制");
-    copyButton->setStyleSheet("QPushButton{ border-image:url(:/icons/copy.png); }"
-                              "QPushButton:hover{ border-image:url(:/icons/copy_hover.png); }");
-    connect(copyButton, &QPushButton::clicked, copyFun);
-    funLay->addWidget(copyButton);
+    copyImagesPath = imagesDir + "/copy.png";
+    copyHoverImagesPath = imagesDir + "/copy_hover.png";
+    copyButton->setStyleSheet(QString("QPushButton{ border-image:url(%1); }"
+                                      "QPushButton:hover{ border-image:url(%2); }")
+                                      .arg(copyImagesPath, copyHoverImagesPath));
+    connect(copyButton, &QPushButton::clicked, this->copyFun);
+    funHLayout->addWidget(copyButton);
     copyButton->hide();
 
     if (!isUser) {
-        renewButton = new QPushButton(funWidget);
-        renewButton->setFixedSize(16, 16);
-        renewButton->setToolTip("重新生成响应");
-        renewButton->setStyleSheet(
-                "QPushButton{ border-image:url(:/icons/renewResponse.png); }"
-                "QPushButton:hover{ border-image:url(:/icons/renewResponse_hover.png); }");
-        connect(renewButton, &QPushButton::clicked, renewFun);
-        funLay->addWidget(renewButton);
-        renewButton->hide();
-        renewButtonIsRemove = false;
+        renewResponseButton = new PushButton("重新生成响应", 25, 35);
+        renewResponseButton->setFixedSize(16, 16);
+        renewResponseImagesPath = imagesDir + "/renewResponse.png";
+        renewResponseHoverImagesPath = imagesDir + "/renewResponse_hover.png";
+        renewResponseButton->setStyleSheet(
+                QString("QPushButton{ border-image:url(:/icons/renewResponse.png); }"
+                        "QPushButton:hover{ border-image:url(:/icons/renewResponse_hover.png); }")
+                        .arg(renewResponseImagesPath, renewResponseHoverImagesPath));
+        connect(renewResponseButton, &QPushButton::clicked, this->renewResponseFun);
+        funHLayout->addWidget(renewResponseButton);
+        renewResponseButton->hide();
+        funHLayout->setSpacing(10);
         funWidget->setFixedSize(52, 26);
+        renewResponseButtonIsRemove = false;
     } else {
         funWidget->setFixedSize(26, 26);
     }
@@ -89,21 +95,152 @@ MessageWidget::MessageWidget(const QString &text, std::function<void()> copyFun,
     else
         buildAiUi();
 
-    auto *mainLay = new QHBoxLayout(this);
-    mainLay->setContentsMargins(0, 0, 0, 0);
-    mainLay->setSpacing(5);
+    auto *mainHLayout = new QHBoxLayout(this);
+    mainHLayout->setContentsMargins(0, 0, 0, 0);
+    mainHLayout->setSpacing(5);
     if (isUser) {
-        mainLay->addWidget(textBoxWidget);
-        mainLay->addWidget(imageLabel);
+        mainHLayout->addWidget(textBoxWidget);
+        mainHLayout->addWidget(imageLabel);
     } else {
-        mainLay->addWidget(imageLabel);
-        mainLay->addWidget(textBoxWidget);
+        mainHLayout->addWidget(imageLabel);
+        mainHLayout->addWidget(textBoxWidget);
     }
     setFixedSize(imageLabel->width() + textBoxWidget->width() + 5,
                  qMax(imageLabel->height(), textBoxWidget->height()));
 }
 
 MessageWidget::~MessageWidget() { }
+
+void MessageWidget::buildUserUi()
+{
+    auto *ts = new TextShow(text, true, textMaxWidth - imageLabel->width() - 15, this);
+    textLayout->addWidget(ts);
+    textWidget->setFixedSize(ts->width() + 10, ts->height());
+    textBoxLayout->addWidget(textWidget);
+    textBoxLayout->addWidget(funWidget);
+    textBoxWidget->setFixedSize(qMax(textWidget->width(), funWidget->width()),
+                                textWidget->height() + funWidget->height());
+}
+
+void MessageWidget::buildAiUi()
+{
+    QString think, result;
+    parseThinkAndResult(text, think, result, thinkTextIsRecvEnd);
+
+    thinkBackWidget = new ThinkBackWidget(this);
+    thinkBackVLayout = new QVBoxLayout(thinkBackWidget);
+    thinkBackVLayout->setContentsMargins(30, 0, 15, 0);
+    thinkBackVLayout->setSpacing(0);
+
+    if (!think.isEmpty() && !think.contains("</think>")) {
+        auto blocks = extractCodeBlocks(think);
+        QStringList splitTexts;
+        QString temp = think;
+        for (const auto &b : blocks) {
+            int idx = temp.indexOf(QString("```%1\n").arg(b.language));
+            splitTexts.append(temp.left(idx));
+            temp = temp.mid(idx + QString("```%1\n").arg(b.language).size() + b.code.size() + 3);
+            auto *code =
+                    new CodeShow(b.code, b.language, textMaxWidth - imageLabel->width() - 80, this);
+            code->connectCodeCopyButtonClick(copyFun);
+            thinkCodeShowList.append(code);
+        }
+        splitTexts.append(temp);
+        for (const auto &t : splitTexts) {
+            if (!t.isEmpty())
+                thinkTextShowList.append(
+                        new TextShow(t, false, textMaxWidth - imageLabel->width() - 80, this));
+        }
+        thinkButton = new ThinkingButton(this);
+        thinkButton->setIsShowThinkContent(thinkIsExpand);
+        connect(thinkButton, &ThinkingButton::clicked, this, &MessageWidget::thinkButtonClicked);
+        textLayout->addWidget(thinkButton);
+        for (int i = 0; i < thinkTextShowList.size(); ++i) {
+            thinkBackVLayout->addWidget(thinkTextShowList[i]);
+            if (i < thinkCodeShowList.size())
+                thinkBackVLayout->addWidget(thinkCodeShowList[i]);
+        }
+        textLayout->addWidget(thinkBackWidget);
+        thinkBackWidget->setVisible(thinkIsExpand);
+        if (thinkTextIsRecvEnd && isRecvFirst) {
+            thinkButton->setThinkEnd();
+            isRecvFirst = false;
+        }
+    }
+
+    if (!result.isEmpty()) {
+        auto blocks = extractCodeBlocks(result);
+        QString temp = result;
+        QStringList splitTexts;
+        for (const auto &b : blocks) {
+            int idx = temp.indexOf(QString("```%1\n").arg(b.language));
+            splitTexts.append(temp.left(idx));
+            temp = temp.mid(idx + QString("```%1\n").arg(b.language).size() + b.code.size() + 3);
+            auto *code =
+                    new CodeShow(b.code, b.language, textMaxWidth - imageLabel->width() - 35, this);
+            code->connectCodeCopyButtonClick(copyFun);
+            resultCodeShowList.append(code);
+        }
+        splitTexts.append(temp);
+        for (const auto &t : splitTexts) {
+            if (!t.isEmpty())
+                resultTextShowList.append(
+                        new TextShow(t, false, textMaxWidth - imageLabel->width() - 35, this));
+        }
+        int j = 0;
+        for (int i = 0; i < resultCodeShowList.size(); ++i) {
+            if (i < splitTexts.size() && !splitTexts[i].isEmpty())
+                textLayout->addWidget(resultTextShowList[i - j]);
+            else
+                ++j;
+            textLayout->addWidget(resultCodeShowList[i]);
+        }
+        if (!splitTexts.isEmpty() && !splitTexts.last().isEmpty())
+            textLayout->addWidget(resultTextShowList.last());
+    }
+
+    loadingWidget = new LoadingWidget(this);
+    textBoxLayout->addWidget(textWidget);
+    textBoxLayout->addWidget(loadingWidget);
+    textBoxWidget->setFixedSize(qMax(textWidget->width(), loadingWidget->width()),
+                                textWidget->height() + loadingWidget->height());
+    loadingWidgetIsRemove = false;
+}
+
+QList<MessageWidget::CodeBlock> MessageWidget::extractCodeBlocks(const QString &text)
+{
+    QList<CodeBlock> list;
+    QRegularExpression re("```(\\w+)\\n(.*?)```", QRegularExpression::DotMatchesEverythingOption);
+    auto it = re.globalMatch(text);
+    while (it.hasNext()) {
+        auto m = it.next();
+        QString lang = m.captured(1);
+        QString code = m.captured(2);
+        if (lang == "cpp" || lang == "python" || lang == "glsl" || lang == "lua")
+            list.append({ lang, code, true });
+    }
+    return list;
+}
+
+void MessageWidget::parseThinkAndResult(const QString &txt, QString &think, QString &result,
+                                        bool &thinkEnd)
+{
+    thinkEnd = false;
+    if (txt.contains("<think>")) {
+        int p1 = txt.indexOf("<think>") + 7;
+        int p2 = txt.indexOf("</think>", p1);
+        if (p2 > 0) {
+            think = txt.mid(p1, p2 - p1);
+            result = txt.mid(p2 + 8);
+            thinkEnd = true;
+        } else {
+            think = txt.mid(p1);
+        }
+    } else if (!txt.contains("<think>") && !txt.isEmpty()) {
+        result = txt;
+        thinkEnd = true;
+    }
+}
 
 bool MessageWidget::getIsUser()
 {
--- qt_chat/messagewidget.h
+++ qt_chat/messagewidget.h
@@ -1,14 +1,14 @@
 #ifndef MESSAGEWIDGET_H
-#define MESSAGEWIDGET_H
+#  define MESSAGEWIDGET_H
 
-#pragma once
-#include "listwidget.h"
-#include "textshow.h"
+#  pragma once
+#  include "listwidget.h"
+#  include "textshow.h"
 
-#include <QtWidgets/QWidget>
-#include <QtWidgets/QHBoxLayout>
-#include <QtWidgets/QLabel>
-#include <QtCore/QString>
+#  include <QtWidgets/QWidget>
+#  include <QtWidgets/QHBoxLayout>
+#  include <QtWidgets/QLabel>
+#  include <QtCore/QString>
 
 class MessageWidget : public QWidget
 {
@@ -81,3 +81,107 @@ void MessageWidget::connectExecuteNext(T *receiver, void (T::*slot)())
 }
 
 #endif // MESSAGEWIDGET_H
+
+#pragma once
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QPushButton>
+#include <QTimer>
+#include <QRegularExpression>
+
+QT_BEGIN_NAMESPACE
+class TextShow;
+class CodeShow;
+class ThinkingButton;
+class LoadingWidget;
+class ImageLabel;
+QT_END_NAMESPACE
+
+extern const QString imagesDir;
+
+class MessageWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit MessageWidget(const QString &text, std::function<void()> copyFun,
+                           std::function<void()> renewResponseFun, bool isUser = true,
+                           int textMaxWidth = 877, QWidget *parent = nullptr);
+    ~MessageWidget();
+
+    void setText(const QString &text);
+    QString getText() const { return text; }
+    bool getIsUser() const { return isUser; }
+
+    void setSize();
+    void removeLoadingWidget();
+    void showFunWidget();
+    void hideFunWidget();
+
+signals:
+    void resizeFinished();
+    void setTexting(bool);
+
+private slots:
+    void onSizeFinished();
+    void onAiUpdateSize();
+    void thinkButtonClicked();
+
+private:
+    void buildUserUi();
+    void buildAiUi();
+    void parseThinkAndResult(const QString &txt, QString &think, QString &result, bool &thinkEnd);
+    QList<CodeBlock> extractCodeBlocks(const QString &text);
+    void updateFunWidgetSize(int curDpi, int initDpi);
+
+    QString copyImagesPath;
+    QString copyHoverImagesPath;
+    QString renewResponseImagesPath;
+    QString renewResponseHoverImagesPath;
+
+    struct CodeBlock
+    {
+        QString language;
+        QString code;
+        bool hasEndMarker;
+    };
+
+    QString text;
+    bool isUser;
+    int textMaxWidth;
+    std::function<void()> copyFun;
+    std::function<void()> renewResponseFun;
+
+    bool thinkButtonHaveCreated = false;
+    QString thinkText;
+    QString resultText;
+    bool thinkTextIsRecvEnd = false;
+    bool isRecvFirst = true;
+    bool thinkIsExpand = true;
+
+    ImageLabel *imageLabel = nullptr;
+    TextWidget *textWidget = nullptr;
+    QVBoxLayout *textLayout = nullptr;
+    TextBoxWidget *textBoxWidget = nullptr;
+    QVBoxLayout *textBoxLayout = nullptr;
+
+    QWidget *funWidget = nullptr;
+    CopyButton *copyButton = nullptr;
+    PushButton *renewResponseButton = nullptr;
+    bool funWidgetIsShow = false;
+    bool loadingWidgetIsRemove = true;
+    bool renewResponseButtonIsRemove = true;
+
+    ThinkingButton *thinkButton = nullptr;
+    ThinkBackWidget *thinkBackWidget = nullptr;
+    QVBoxLayout *thinkBackVLayout = nullptr;
+    LoadingWidget *loadingWidget = nullptr;
+
+    QList<TextShow *> thinkTextShowList;
+    QList<CodeShow *> thinkCodeShowList;
+    QList<TextShow *> resultTextShowList;
+    QList<CodeShow *> resultCodeShowList;
+
+    QTimer aiUpdateSizeTimer;
+};
